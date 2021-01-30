0f719e9a3384c47db84b2e5929d0c40c853455f7
Hiroyuki Ohsaki <code@lsnl.jp>
2021-01-30 22:54:51 +0900

    version 0.4, support COMET II/CASL II

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name="casl",
-    version="0.3",
+    version="0.4",
     author="Hiroyuki Ohsaki",
     author_email="ohsaki@lsnl.jp",
     description="a simple implementation of CASL assembler/COMET simulator in Python",
