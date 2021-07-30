66c62efa4028f3fd7b442a3d8b551588aeea9a87
Hiroyuki Ohsaki <code@lsnl.jp>
2021-07-30 23:44:30 +0900

    version 1.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name="casl",
-    version="0.9",
+    version="1.0",
     author="Hiroyuki Ohsaki",
     author_email="ohsaki@lsnl.jp",
     description="a simple implementation of CASL assembler/COMET simulator in Python",
